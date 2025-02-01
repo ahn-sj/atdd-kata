97b12e26d997bdf5d278dec8e248a23d5af355ad
ahn-sj <jaesa5221@gmail.com>
2025-02-01 22:41:12 +0900

    feat: step6 - Outside In Register OrderService Bean

--- src/main/java/atdd/application/CreateOrderServiceImpl.java
+++ src/main/java/atdd/application/CreateOrderServiceImpl.java
@@ -3,7 +3,9 @@ package atdd.application;
 import atdd.domain.PendingOrder;
 import atdd.domain.PendingOrderRepository;
 import atdd.infra.PendingOrderInMemoryRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 class CreateOrderServiceImpl implements CreateOrderService {
     private PendingOrderRepository pendingOrderRepository = new PendingOrderInMemoryRepository();
 
