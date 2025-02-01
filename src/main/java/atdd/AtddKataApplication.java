c3df7fbc57a7a317cdf5ba5aa0da1baa72943030
ahn-sj <jaesa5221@gmail.com>
2025-02-01 17:23:03 +0900

    Initial commit

---
+++ src/main/java/atdd/AtddKataApplication.java
@@ -0,0 +1,13 @@
+package atdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddKataApplication.class, args);
+    }
+
+}
