10ccfbd5e492b61cf99e2a6b290e12580e70f6b9
ahn-sj <jaesa5221@gmail.com>
2025-02-01 22:44:08 +0900

    feat: step8 - Outside In Register OrderRepository Bean

--- src/main/java/atdd/infra/PendingOrderInMemoryRepository.java
+++ src/main/java/atdd/infra/PendingOrderInMemoryRepository.java
@@ -2,9 +2,11 @@ package atdd.infra;
 
 import atdd.domain.PendingOrder;
 import atdd.domain.PendingOrderRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class PendingOrderInMemoryRepository implements PendingOrderRepository {
     private final AtomicLong atomicId = new AtomicLong(1);
 
