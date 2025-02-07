af8f2fb0761d07a4ad9151a9c455592b7ed61ab3
ahn-sj <jaesa5221@gmail.com>
2025-02-07 20:19:12 +0900

    Initial commit

---
+++ atdd-booker/src/main/java/booker/AtddBookerApplication.java
@@ -0,0 +1,13 @@
+package booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddBookerApplication.class, args);
+    }
+
+}
